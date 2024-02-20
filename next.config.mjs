5ba2599134217b4b26bb07472b50f422aae9ed13
Sina Shafiee <alisinashafieezada@gmail.com>
2024-02-20 17:14:13 +0330

    feat(config): svgr

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,35 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		addSvgrRules(config);
+		return config;
+	},
+};
 
+const addSvgrRules = (config) => {
+	const fileLoaderRule = config.module.rules.find((rule) =>
+		rule.test?.test?.(".svg"),
+	);
+
+	config.module.rules.push(
+		{
+			...fileLoaderRule,
+			test: /\.svg$/i,
+			resourceQuery: /url/,
+		},
+		{
+			test: /\.svg$/i,
+			issuer: { not: /\.(css|scss|sass)$/ },
+			resourceQuery: { not: /url/ },
+			loader: "@svgr/webpack",
+			options: {
+				typescript: true,
+			},
+		},
+	);
+
+	fileLoaderRule.exclude = /\.svg$/i;
+};
 export default withNextIntl(nextConfig);
