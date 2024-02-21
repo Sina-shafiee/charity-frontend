fcf72fb6200a400012ef75ee43df69a2fd58be8b
Sina Shafiee <alisinashafieezada@gmail.com>
2024-02-21 16:58:53 +0330

    fix: not-found error fix
    fix #4

---
+++ src/app/[locale]/[...catch-all]/page.tsx
@@ -0,0 +1,9 @@
+/**
+ * dirty Fix for https://github.com/Sina-shafiee/charity-frontend/issues/1
+ * TODO implement not found page
+ */
+const NotFound = () => {
+	return <div>NotFound</div>;
+};
+
+export default NotFound;
