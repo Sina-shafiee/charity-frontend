034c0f45f69aea4c5a6f89c7e29b81245e79212f
Sina Shafiee <alisinashafieezada@gmail.com>
2024-02-23 20:05:39 +0330

    fix(ui): typography colors

--- src/component/domain/landing/hero.tsx
+++ src/component/domain/landing/hero.tsx
@@ -13,12 +13,12 @@ export const Hero = async () => {
 		<section className="relative grid h-screen max-h-[840px] place-content-center bg-[url('/images/landing-hero-pattern.webp')] bg-cover bg-center bg-no-repeat">
 			<RightPath className="absolute bottom-0 right-0 h-auto w-80" />
 			<LeftPath className="absolute bottom-0 left-0 h-72 w-auto" />
-			<section className="relative z-40 mt-28 flex w-full flex-col text-center font-semibold text-white">
-				<h5 className={cn(isRtl ? "mb-4" : "mb-2")}>{t("h5")}</h5>
+			<section className="relative z-40 mt-28 flex w-full flex-col text-center font-semibold">
+				<h5 className={cn(isRtl ? "mb-4" : "mb-2", "text-white")}>{t("h5")}</h5>
 				<h1
 					className={cn(
 						isRtl ? "max-w-[840px]" : "max-w-[770px]",
-						"mx-auto flex flex-col text-[72px] capitalize leading-[90px]",
+						"mx-auto flex flex-col text-white text-[72px] capitalize leading-[90px]",
 					)}
 				>
 					{t("heading")}
@@ -26,7 +26,7 @@ export const Hero = async () => {
 				<h6
 					className={cn(
 						isRtl ? "mt-6" : "mt-4",
-						"mx-auto  max-w-[600px] font-medium",
+						"mx-auto text-white max-w-[600px] font-medium",
 					)}
 				>
 					{t("h6")}
@@ -35,7 +35,7 @@ export const Hero = async () => {
 					<Button className="max-w-max">
 						<Link href="/causes">{t("main-cta")}</Link>
 					</Button>
-					<Button variant="outline" className="max-w-max" asChild>
+					<Button variant="outline" className="max-w-max text-white" asChild>
 						<Link href="/about">{t("secondary-cta")}</Link>
 					</Button>
 				</div>
