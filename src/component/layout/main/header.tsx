f2952dbd8523556302a9016be4615883f3751447
Sina Shafiee <alisinashafieezada@gmail.com>
2024-02-24 11:18:39 +0330

    feat(landing): navbar login button & hover effect for nav links

--- src/component/layout/main/header.tsx
+++ src/component/layout/main/header.tsx
@@ -33,7 +33,7 @@ export const Header = async () => {
 					<Logo className="h-[33px] w-[133px]" />
 				</Link>
 				<Navbar links={translatedLinks} />
-				<div className="flex items-center gap-4">
+				<div className="flex items-center gap-2">
 					<Button size="sm" className="text-base">
 						{t("login")}
 					</Button>
--- src/component/layout/main/navbar.tsx
+++ src/component/layout/main/navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useDirection } from "@radix-ui/react-direction";
 import Link from "next/link";
 
+import { cn } from "@/utils";
 import type { NavbarLink } from "@/utils/constant";
 
 interface Props {
@@ -9,13 +11,27 @@ interface Props {
 }
 
 export const Navbar = ({ links }: Props) => {
+	const isRtl = useDirection() === "rtl";
+
 	return (
 		<nav>
-			<ul className="mt-1 flex items-center gap-x-4">
+			<ul className="mt-1 flex items-center gap-x-5">
 				{links.map((link) => {
 					return (
-						<li key={link.href} className="text-base font-medium">
-							<Link href={link.href}>{link.title}</Link>
+						<li
+							key={link.href}
+							className="group text-lg text-white transition-all duration-300 ease-in-out"
+						>
+							<Link href={link.href}>
+								<span
+									className={cn(
+										isRtl ? "bg-right-bottom" : "bg-left-bottom",
+										"bg-gradient-to-l group-hover:bg-[length:100%_3px] bg-[length:0%_3px] bg-no-repeat transition-all duration-500 ease-out pb-2 from-blue-500 to-blue-500",
+									)}
+								>
+									{link.title}
+								</span>
+							</Link>
 						</li>
 					);
 				})}
