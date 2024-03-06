a52d1e1bb8b968b67f155b46fe66f55d9d6b689b
Sina Shafiee <alisinashafieezada@gmail.com>
2024-03-06 17:07:28 +0330

    feat(ui): page navigation loading indicator

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { DirectionProvider } from "@radix-ui/react-direction";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 import type { ReactNode } from "react";
 
 import { Toaster } from "@/component/ui";
@@ -19,6 +20,15 @@ const Providers = ({ children, dir, locale }: Props) => {
 		<SessionProvider>
 			<I18nProviderClient locale={locale}>
 				<DirectionProvider dir={dir}>
+					<NextTopLoader
+						color="#219c7f"
+						initialPosition={0.08}
+						crawlSpeed={200}
+						crawl
+						showSpinner={false}
+						shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+						zIndex={1600}
+					/>
 					{children}
 					<Toaster />
 				</DirectionProvider>
