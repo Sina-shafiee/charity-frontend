ac3cb69cfed697a8aa87da6e26a8d101a4c90468
Sina Shafiee <alisinashafieezada@gmail.com>
2024-03-12 19:44:28 +0330

    fix(auth): cookie

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -32,7 +32,7 @@ export const sessionOptions: SessionOptions = {
 	ttl: Number.parseInt(env.SESSION_TTL, 10),
 	cookieOptions: {
 		httpOnly: true,
-		secure: env.NODE_ENV === "production",
+		secure: false,
 		sameSite: "lax",
 		path: "/",
 	},
